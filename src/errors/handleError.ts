3437e6eff2a7bb9cd15f8f391167832aa6def436
Pedro Ribeiro <86488494+pedroheribeiro2021@users.noreply.github.com>
2024-03-26 14:57:46 -0300

    backup

--- src/errors/handleError.ts
+++ src/errors/handleError.ts
@@ -1,19 +1,21 @@
-import { Request, Response } from "express";
-import { AppError } from "./AppError";
+import { Request, Response } from "express"
+import { AppError } from "./AppError"
 
-
-export const handleError = async (error: Error, req: Request, res: Response) => {
-
-    if(error instanceof AppError){
-        console.log(error)
-        return res.status(error.statusCode).json({
-            message: error.message
-        })
-    }
-
+export const handleError = async (
+  error: Error,
+  req: Request,
+  res: Response,
+) => {
+  if (error instanceof AppError) {
     console.log(error)
-
-    return res.status(500).json({
-        message: 'Internal server error'
+    return res.status(error.statusCode).json({
+      message: error.message,
     })
+  }
+
+  console.log(error)
+
+  return res.status(500).json({
+    message: "Internal server error",
+  })
 }
