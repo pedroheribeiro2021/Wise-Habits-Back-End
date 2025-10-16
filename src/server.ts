57896c152b421598ac643cc8b93cbdffb904ce08
PEDRO HENRIQUE SILVA RIBEIRO <pedro.ribeiro@centralit.com.br>
2025-10-16 17:35:11 -0300

    render

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import { AppDataSource } from "./data-source"
 AppDataSource.initialize()
   .then(() => {
     console.log("Database connected!")
-    app.listen(3003, () => {
+    // app.listen(3003, () => {
+    //   console.log("Server is running!")
+    // })
+    app.listen(process.env.PORT || 3000, () => {
       console.log("Server is running!")
     })
   })
